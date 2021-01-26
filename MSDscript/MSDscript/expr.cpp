8467971c43337e6d284f2314f72746274f2d4662
Taylor <taylorjodunn@gmail.com>
2021-01-26 14:15:39 -0700

    added interpret, has varible, and substitution functions and tests

--- MSDscript/MSDscript/expr.cpp
+++ MSDscript/MSDscript/expr.cpp
@@ -7,6 +7,7 @@
 
 #include "expr.hpp"
 #include "catch.h"
+#include <stdexcept>
 
 Num::Num(int val){
     this->val = val;
@@ -19,6 +20,18 @@ bool Num::equals(Expr *other){
     } else {
         return (this->val == other_num->val);
     }
+}
+
+int Num::interp(){
+    return this->val;
+}
+
+bool Num::has_variable(){
+    return false;
+}
+
+Expr* Num::subst(std::string string, Expr *a){
+    return this;
 }
 
 Add::Add(Expr *lhs, Expr *rhs){
@@ -35,6 +48,20 @@ bool Add::equals(Expr *other){
     }
 }
 
+int Add::interp(){
+    return (this->lhs->interp() + this->rhs->interp());
+}
+
+bool Add::has_variable(){
+    return (this->lhs->has_variable() || this->rhs->has_variable());
+}
+
+Expr* Add::subst(std::string string, Expr *a){
+    this->lhs = lhs->subst(string, a);
+    this->rhs = rhs->subst(string, a);
+    return this;
+}
+
 Mult::Mult(Expr *lhs, Expr *rhs){
     this->lhs = lhs;
     this->rhs = rhs;
@@ -49,6 +76,20 @@ bool Mult::equals(Expr *other){
     }
 }
 
+int Mult::interp(){
+    return (this->lhs->interp() * this->rhs->interp());
+}
+
+bool Mult::has_variable(){
+    return (this->lhs->has_variable() || this->rhs->has_variable());
+}
+
+Expr* Mult::subst(std::string string, Expr *a){
+    this->lhs = lhs->subst(string, a);
+    this->rhs = rhs->subst(string, a);
+    return this;
+}
+
 Variable::Variable(std::string val){
     this->val = val;
 }
@@ -59,6 +100,22 @@ bool Variable::equals(Expr *other){
         return false;
     } else {
         return (this->val == other_var->val);
+    }
+}
+
+int Variable::interp(){
+    throw std::runtime_error("No value for variable");
+}
+
+bool Variable::has_variable(){
+    return true;
+}
+
+Expr* Variable::subst(std::string string, Expr *a){
+    if(this->val == string){
+        return a;
+    } else {
+        return this;
     }
 }
 
@@ -119,4 +176,40 @@ TEST_CASE ( "Object Comparisions" ){
     CHECK((new Add(new Num(0),new Num(1)))->equals(new Variable("%")) == false);
     CHECK((new Mult(new Num(0),new Num(1)))->equals(new Num(7)) == false);
     CHECK((new Variable("*"))->equals(new Mult(new Num(0),new Num(1))) == false);
+}
+
+
+TEST_CASE ( "Interpret" ){
+    CHECK((new Num(5))->interp() == 5);
+    CHECK((new Num(-1))->interp() == -1);
+    CHECK((new Num(0))->interp() == 0);
+    CHECK((new Add(new Num(2),new Num(4)))->interp() == 6);
+    CHECK((new Add(new Num(-10),new Num(7)))->interp() == -3);
+    CHECK((new Add(new Num(-31),new Num(31)))->interp() == 0);
+    CHECK((new Mult(new Num(2),new Num(4)))->interp() == 8);
+    CHECK((new Mult(new Num(-5),new Num(-2)))->interp() == 10);
+    CHECK((new Mult(new Num(5),new Num(-2)))->interp() == -10);
+    CHECK_THROWS_WITH( (new Variable("x"))->interp(), "No value for variable" );
+    CHECK_THROWS_WITH( (new Variable("&"))->interp(), "No value for variable" );
+    CHECK_THROWS_WITH( (new Variable(""))->interp(), "No value for variable" );
+    CHECK_THROWS_WITH( (new Add(new Variable("b"),new Num(31)))->interp(), "No value for variable" );
+    CHECK_THROWS_WITH( (new Mult(new Num(3),new Variable("a")))->interp(), "No value for variable" );
+}
+
+TEST_CASE ( "Has Variable" ){
+    CHECK(((new Num(5))->has_variable() == false));
+    CHECK((new Add(new Num(2),new Num(4)))->has_variable()== false);
+    CHECK((new Add(new Num(2),new Variable("&")))->has_variable() == true);
+    CHECK((new Mult(new Num(2),new Num(4)))->has_variable()== false);
+    CHECK((new Mult(new Variable("/"),new Num(4)))->has_variable()== true);
+    CHECK((new Variable("*"))->has_variable() == true);
+}
+
+TEST_CASE ( "Substitution" ){
+    CHECK((new Num(1))->subst("a", new Num(4))->equals(new Num(1)) == true);
+    CHECK((new Num(1))->subst("a", new Num(4))->equals(new Num(4)) == false);
+    CHECK((new Variable("a"))->subst("a", new Variable("c"))->equals(new Variable("c"))== true);
+    CHECK((new Variable("a"))->subst("b", new Variable("c"))->equals(new Variable("a"))== true);
+    CHECK((new Mult(new Variable("x"), new Num(7)))->subst("x", new Variable("y"))->equals(new Mult(new Variable("y"), new Num(7)))==true);
+    CHECK((new Add(new Variable("x"), new Num(7)))->subst("x", new Variable("y"))->equals(new Add(new Variable("y"), new Num(7)))==true);
 }
--- MSDscript/MSDscript/expr.hpp
+++ MSDscript/MSDscript/expr.hpp
@@ -13,7 +13,14 @@
 
 class Expr{
 public:
+    //Returns whether the calling expression is equal to the parameter expression.
     virtual bool equals(Expr *a) = 0;
+    //Returns the value of an expression. If the expression is a variable, throws a runtime error.
+    virtual int interp() = 0;
+    //Returns whether the expression contains a variable.
+    virtual bool has_variable() = 0;
+    //Returns everywhere that the calling expression contained the parameter string and replaces it with the parameter expression.
+    virtual Expr* subst(std::string string, Expr *a) = 0;
 };
 
 class Num: public Expr {
@@ -22,6 +29,9 @@ public:
     
     Num(int val);
     bool equals(Expr *other);
+    int interp();
+    bool has_variable();
+    Expr* subst(std::string string, Expr *a);
 };
 
 class Add: public Expr{
@@ -31,6 +41,9 @@ public:
     
     Add(Expr *lhs, Expr *rhs);
     bool equals(Expr *other);
+    int interp();
+    bool has_variable();
+    Expr* subst(std::string string, Expr *a);
 };
 
 class Mult: public Expr{
@@ -40,6 +53,9 @@ public:
     
     Mult(Expr *lhs, Expr *rhs);
     bool equals(Expr *other);
+    int interp();
+    bool has_variable();
+    Expr* subst(std::string string, Expr *a);
 };
 
 class Variable: public Expr{
@@ -48,6 +64,9 @@ public:
     
     Variable(std::string val);
     bool equals(Expr *other);
+    int interp();
+    bool has_variable();
+    Expr* subst(std::string string, Expr *a);
 };
 
 #endif /* expr_hpp */
